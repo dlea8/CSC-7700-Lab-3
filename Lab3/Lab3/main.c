288244211788284a4ee595190431450fd87e894e
Tre Landaiche <tre.landaiche@gmail.com>
2025-02-19 10:37:01 -0600

    Made it into a Microchip Studio project :)

---
+++ Lab3/Lab3/main.c
@@ -0,0 +1,19 @@
+/*
+ * Lab3.c
+ *
+ * Created: 2/19/2025 10:30:01 AM
+ * Author : trebu
+ */ 
+
+#include <avr/io.h>
+#include "helper/haha_v3_xmega.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
